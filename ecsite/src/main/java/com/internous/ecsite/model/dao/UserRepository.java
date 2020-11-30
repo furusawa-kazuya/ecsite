e60a26deaf0b91a41d86840064e889d7dfe025c4
furusawa-kazuya <kazuya1993@outlook.com>
2020-11-30 18:38:53 +0900

    STS4.8で作成しなおしたECサイト

---
+++ ecsite/src/main/java/com/internous/ecsite/model/dao/UserRepository.java
@@ -0,0 +1,18 @@
+package com.internous.ecsite.model.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.internous.ecsite.model.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	List<User> findByUserNameAndPassword(String userName, String password);
+	
+	// userNameが一致するレコードが何件あるか確認用。
+	@Query(value="SELECT count(id) FROM user WHERE user_name = :userName", nativeQuery=true)
+	int findCountByUserName(@Param("userName") String userName);
+}
